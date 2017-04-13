25ac0fe6d27682e98d38737a7b29a0e159bd43fc
zf101115 <275804880@qq.com>
2017-04-13 11:54:48 +0800

    set layout

--- Ladder_Android/app/src/main/java/com/example/yogaxiong/ladder/MainActivity.java
+++ Ladder_Android/app/src/main/java/com/example/yogaxiong/ladder/MainActivity.java
@@ -36,20 +36,20 @@ public class MainActivity extends AppCompatActivity {
         config();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void config() {
         setContentView(R.layout.activity_main);
